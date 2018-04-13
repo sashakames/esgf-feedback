9228cec8d1cdb65bd6e6ccdde029a9fb9b2e814a
Sasha Ames <amysash2006@gmail.com>
2018-04-13 09:46:02 -0700

    update and refactor

--- send_job.py
+++ send_job.py
@@ -26,6 +26,44 @@ def format_email(user, user_action_dict):
         return outs
 
 
+def process_users(user_dict):
+
+    for user in user_dict:
+
+        user_items = user_dict[user]
+
+        # Lookup user email - bulk query or single user?
+
+        # for now hardcoded email
+        
+        user_action_dict = {}
+        
+        for its in user_items:
+
+            action = its["update_status"]
+
+            if action in user_action_dict:
+                
+                dset_lst = user_action_dict[action]
+                dset_lst.append(its["master_id"])
+
+            else:                
+                user_action_dict[action] = its["master_id"]
+        
+
+            
+        dest_addr = addr_lookup.get(user)
+        subject = "ESGF Datatset status updates"
+
+        body = format_email(user, user_action_dict)
+
+        if (len(sys.argv) > 1 and sys.argv[1] == "--test"):
+            print dest_addr, subject, body 
+        else:
+            email_notify(dest_addr, subject, body)
+
+
+
 def main():
 
     logger = logging.getLogger('esgf-feedback')
@@ -61,53 +99,23 @@ def main():
             action = it["action"]
 
             for usr in it["users"]:
+                record = { 'update_status': action , 'master_id': dset }               
 
                 if usr in user_dict:
 
                     dset_lst = user_dict[usr]
-                    dset_lst.append([dset, action])
+
+                    dset_lst.append(record)
                     user_dict[usr] = dset_lst
                 else:
-                    user_dict[usr] = [[dset, action]]
+                    user_dict[usr] = [record]
+
 
         out_log_arr.append(jobj)
         os.remove(LOOK_PATH + "/" + ff)
 
-    for user in user_dict:
 
-        user_items = user_dict[user]
-
-        # Lookup user email - bulk query or single user?
-
-        # for now hardcoded email
-        
-        user_action_dict = {}
-        
-        for its in user_items:
-
-            action = its[0]
-
-            if action in user_action_dict:
-                
-                dset_lst = user_action_dict[action]
-                dset_lst.append(its[1])
-
-            else:                
-                user_action_dict[action] = its[1]
-        
-
-            
-        dest_addr = addr_lookup.get(user)
-        subject = "ESGF Datatset status updates"
-
-        body = format_email(user, user_action_dict)
-
-        if (len(sys.argv) > 1 and sys.argv[1] == "--test"):
-            print dest_addr, subject, body 
-        else:
-            email_notify(dest_addr, subject, body)
-
-
+    process_users(user_dict)
 
     os.rmdir(LOOK_PATH)
     open(LOG_FILE, 'w').write(json.dumps(out_log_arr))
